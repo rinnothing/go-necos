4fe040235854e6382a2df9bb22938751ab024986
rinnothing <58170199+rinnothing@users.noreply.github.com>
2024-10-26 22:47:47 +0300

    feat: reedited tests

--- api_test.go
+++ api_test.go
@@ -8,23 +8,14 @@ import (
 	"testing"
 )
 
-type testCase struct {
-	name   string
-	path   string
-	query  url.Values
-	result string
-}
-
-func makeCase(name string, path string, query url.Values) testCase {
-	return testCase{
-		name:   name,
-		path:   path,
-		query:  query,
-		result: path + query.Encode(),
-	}
-}
-
 func TestGet(t *testing.T) {
+	type testCases struct {
+		name   string
+		path   string
+		query  url.Values
+		result string
+	}
+
 	s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != "GET" {
 			w.WriteHeader(http.StatusMethodNotAllowed)
@@ -39,19 +30,42 @@ func TestGet(t *testing.T) {
 		Domain: s.URL,
 	}
 
-	cases := []testCase{
-		makeCase("empty_root", "", url.Values{}),
-		makeCase("empty_directory", "/directory", url.Values{}),
-		makeCase("query_root", "", url.Values{"oh": {"hello", "there"}}),
-		makeCase("query_directory", "/directory", url.Values{"oh": {"hello", "there"}}),
+	tableTests := []testCases{
+		{
+			name:   "empty_root",
+			path:   "",
+			query:  url.Values{},
+			result: "",
+		},
+		{
+			name:   "empty_directory",
+			path:   "/directory",
+			query:  url.Values{},
+			result: "/directory",
+		},
+		{
+			name:   "query_root",
+			path:   "",
+			query:  url.Values{"oh": {"hello", "there"}},
+			result: "?oh=hello&oh=there",
+		},
+		{
+			name:   "query_directory",
+			path:   "/directory",
+			query:  url.Values{"oh": {"hello", "there"}},
+			result: "/directory?oh=hello&oh=there",
+		},
 	}
 
-	for _, cs := range cases {
-		var answer string
+	for _, cs := range tableTests {
+		t.Run(cs.name, func(t *testing.T) {
+			t.Parallel()
+			var answer string
 
-		err := c.Get(cs.path, cs.query, &answer)
-		require.NoError(t, err)
+			err := c.Get(cs.path, cs.query, &answer)
+			require.NoError(t, err)
 
-		require.Equal(t, cs.result, answer)
+			require.Equal(t, cs.result, answer)
+		})
 	}
 }
