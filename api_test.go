d8b3c7db50eb80ea9e025e65a8da7bfe6c45450a
rinnothing <58170199+rinnothing@users.noreply.github.com>
2024-10-27 14:49:05 +0300

    test: add TestPost test

--- api_test.go
+++ api_test.go
@@ -85,3 +85,79 @@ func TestGet(t *testing.T) {
 		s.Close()
 	}()
 }
+
+func TestPost(t *testing.T) {
+	type testCases struct {
+		name   string
+		path   string
+		query  url.Values
+		result string
+	}
+
+	s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "POST" {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+		}
+
+		ans := r.URL.Path
+		if r.URL.RawQuery != "" {
+			ans += "?" + r.URL.RawQuery
+		}
+		m, _ := json.Marshal(ans)
+		_, _ = w.Write(m)
+	}))
+
+	c := Client{
+		Domain: s.URL,
+	}
+
+	tableTests := []testCases{
+		{
+			name:   "empty_root",
+			path:   "/",
+			query:  url.Values{},
+			result: "/",
+		},
+		{
+			name:   "empty_directory",
+			path:   "/directory",
+			query:  url.Values{},
+			result: "/directory",
+		},
+		{
+			name:   "query_root",
+			path:   "/",
+			query:  url.Values{"oh": {"hello", "there"}},
+			result: "/?oh=hello&oh=there",
+		},
+		{
+			name:   "query_directory",
+			path:   "/directory",
+			query:  url.Values{"oh": {"hello", "there"}},
+			result: "/directory?oh=hello&oh=there",
+		},
+	}
+
+	wg := sync.WaitGroup{}
+	for _, cs := range tableTests {
+		wg.Add(1)
+
+		t.Run(cs.name, func(t *testing.T) {
+			t.Parallel()
+
+			defer wg.Done()
+			var answer string
+
+			err := c.Post(cs.path, cs.query, &answer)
+
+			require.NoError(t, err)
+
+			require.Equal(t, cs.result, answer)
+		})
+	}
+
+	go func() {
+		wg.Wait()
+		s.Close()
+	}()
+}
