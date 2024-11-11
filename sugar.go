b4f04008d08b65f7f9dafb1eccb38f4f0a3a7ca0
rinnothing <58170199+rinnothing@users.noreply.github.com>
2024-11-11 21:41:02 +0300

    fix: replace WriteCloser with Writer

--- sugar.go
+++ sugar.go
@@ -13,8 +13,8 @@ var (
 
 // download is the method used to do all downloads
 //
-// it makes a GET request to given url and writes received content to dst, if all operations finished successfully closes it
-func (c *Client) download(ctx context.Context, url string, dst io.WriteCloser) error {
+// it makes a GET request to given url and writes received content to dst, doesn't close the Writer
+func (c *Client) download(ctx context.Context, url string, dst io.Writer) error {
 	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, http.NoBody)
 	if err != nil {
 		return err
@@ -33,26 +33,33 @@ func (c *Client) download(ctx context.Context, url string, dst io.WriteCloser) e
 	if err != nil {
 		return err
 	}
-	err = dst.Close()
 	return err
 }
 
 // DownloadImage downloads the Image with default context
-func (c *Client) DownloadImage(im *Image, dst io.WriteCloser) error {
+//
+// doesn't close the Writer
+func (c *Client) DownloadImage(im *Image, dst io.Writer) error {
 	return c.DownloadImageWithContext(context.Background(), im, dst)
 }
 
 // DownloadImageWithContext downloads the Image with given context
-func (c *Client) DownloadImageWithContext(ctx context.Context, im *Image, dst io.WriteCloser) error {
+//
+// doesn't close the Writer
+func (c *Client) DownloadImageWithContext(ctx context.Context, im *Image, dst io.Writer) error {
 	return c.download(ctx, im.ImageURL, dst)
 }
 
 // DownloadSample downloads the sample of Image with default context
-func (c *Client) DownloadSample(im *Image, dst io.WriteCloser) error {
+//
+// doesn't close the Writer
+func (c *Client) DownloadSample(im *Image, dst io.Writer) error {
 	return c.DownloadSampleWithContext(context.Background(), im, dst)
 }
 
 // DownloadSampleWithContext downloads the sample of Image with given context
-func (c *Client) DownloadSampleWithContext(ctx context.Context, im *Image, dst io.WriteCloser) error {
+//
+// doesn't close the Writer
+func (c *Client) DownloadSampleWithContext(ctx context.Context, im *Image, dst io.Writer) error {
 	return c.download(ctx, im.SampleURL, dst)
 }
