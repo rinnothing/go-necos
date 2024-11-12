564f72e17e9d48798f4cb57356340cac05742671
rinnothing <58170199+rinnothing@users.noreply.github.com>
2024-11-12 16:20:42 +0300

    feat: make a proper interface for different ImageFile types

--- sugar.go
+++ sugar.go
@@ -1,6 +1,7 @@
 package go_necos
 
 import (
+	"bufio"
 	"bytes"
 	"context"
 	"fmt"
@@ -13,18 +14,119 @@ var (
 	SafeRequest = Request{"rating": []string{"safe"}}
 )
 
-// todo: make a general interface that could allow for files to be read more than once and to Close them (for SaveRam to nil it's slice and for SaveTemp to delete)
+type imageFilePrivate interface {
+	io.ReadWriteCloser
+	Remove() error
+	Save() error
+}
+
+type ImageFile interface {
+	io.ReadCloser
+	Remove() error
+	getPrivate() imageFilePrivate
+}
+
+type DiskFile struct {
+	bufio.ReadWriter
+	f     *os.File
+	saved bool
+}
+
+func NewDiskFile(f *os.File) ImageFile {
+	return &DiskFile{
+		ReadWriter: bufio.ReadWriter{
+			Reader: bufio.NewReader(f),
+			Writer: bufio.NewWriter(f),
+		},
+		f:     f,
+		saved: false,
+	}
+}
+
+func (d *DiskFile) Close() error {
+	if err := d.Flush(); err != nil {
+		return err
+	}
+	return d.f.Close()
+}
+
+func (d *DiskFile) Remove() error {
+	if err := d.Close(); err != nil {
+		return err
+	}
+	return os.Remove(d.f.Name())
+}
+
+func (d *DiskFile) Save() error {
+	if d.saved {
+		return nil
+	}
+
+	if err := d.Flush(); err != nil {
+		return err
+	}
+	d.saved = true
+	return nil
+}
+
+func (d *DiskFile) getPrivate() imageFilePrivate {
+	return d
+}
 
 // Save returns file under given name
-func Save(name string) (*os.File, error) {
-	return os.Create(name)
+func Save(name string) (ImageFile, error) {
+	file, err := os.Create(name)
+	if err != nil {
+		return nil, err
+	}
+
+	return NewDiskFile(file), nil
 }
 
 // SaveTemp returns file created in temporary directory
 //
 // it's the callers responsibility to delete file after use
-func SaveTemp() (*os.File, error) {
-	return os.CreateTemp("", "go-necos")
+func SaveTemp() (ImageFile, error) {
+	file, err := os.CreateTemp("", "go-necos")
+	if err != nil {
+		return nil, err
+	}
+
+	return NewDiskFile(file), nil
+}
+
+type RamFile struct {
+	io.ReadWriter
+	saved bool
+}
+
+func NewRamFile() ImageFile {
+	return &RamFile{
+		ReadWriter: &bytes.Buffer{},
+		saved:      false,
+	}
+}
+
+func (r *RamFile) Close() error {
+	return nil
+}
+
+func (r *RamFile) Remove() error {
+	r.ReadWriter = nil
+	return nil
+}
+
+func (r *RamFile) Save() error {
+	if r.saved {
+		return nil
+	}
+
+	r.saved = true
+	return nil
+}
+
+func (r *RamFile) getPrivate() imageFilePrivate {
+	return r
 }
 
 // SaveRAM return writer that saves it's content to RAM
@@ -34,8 +136,10 @@ func SaveRAM() (*bytes.Buffer, error) {
 
 // download is the method used to do all downloads
 //
-// it makes a GET request to given url and writes received content to dst, doesn't close the Writer
-func (c *Client) download(ctx context.Context, url string, dst io.Writer) error {
+// it makes a GET request to given url and writes received content to dst, saves the content when finished
+func (c *Client) download(ctx context.Context, url string, dstCmn ImageFile) error {
+	dst := dstCmn.getPrivate()
+
 	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, http.NoBody)
 	if err != nil {
 		return err
@@ -50,8 +154,10 @@ func (c *Client) download(ctx context.Context, url string, dst io.Writer) error 
 		return fmt.Errorf("%w: %s", BadStatusError, response.Status)
 	}
 
-	_, err = io.Copy(dst, response.Body)
-	if err != nil {
+	if _, err = io.Copy(dst, response.Body); err != nil {
+		return err
+	}
+	if err = dst.Save(); err != nil {
 		return err
 	}
 	return err
@@ -60,27 +166,27 @@ func (c *Client) download(ctx context.Context, url string, dst io.Writer) error 
 // DownloadImage downloads the Image with default context
 //
 // doesn't close the Writer
-func (c *Client) DownloadImage(im *Image, dst io.Writer) error {
+func (c *Client) DownloadImage(im *Image, dst ImageFile) error {
 	return c.DownloadImageWithContext(context.Background(), im, dst)
 }
 
 // DownloadImageWithContext downloads the Image with given context
 //
 // doesn't close the Writer
-func (c *Client) DownloadImageWithContext(ctx context.Context, im *Image, dst io.Writer) error {
+func (c *Client) DownloadImageWithContext(ctx context.Context, im *Image, dst ImageFile) error {
 	return c.download(ctx, im.ImageURL, dst)
 }
 
 // DownloadSample downloads the sample of Image with default context
 //
 // doesn't close the Writer
-func (c *Client) DownloadSample(im *Image, dst io.Writer) error {
+func (c *Client) DownloadSample(im *Image, dst ImageFile) error {
 	return c.DownloadSampleWithContext(context.Background(), im, dst)
 }
 
 // DownloadSampleWithContext downloads the sample of Image with given context
 //
 // doesn't close the Writer
-func (c *Client) DownloadSampleWithContext(ctx context.Context, im *Image, dst io.Writer) error {
+func (c *Client) DownloadSampleWithContext(ctx context.Context, im *Image, dst ImageFile) error {
 	return c.download(ctx, im.SampleURL, dst)
 }
